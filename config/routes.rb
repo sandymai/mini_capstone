e6bb9329c5524b7330ea47e7420bef1900cc5f41
“Sandy <sandymai0108@gmail.com>
2019-02-26 19:33:26 -0600

    create a route for available products

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/products_list" => "products#products_available"
+  end
 end
