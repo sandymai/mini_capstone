20b9a78f8c7daddee4d3168d227184d5a3fe9653
“Sandy <sandymai0108@gmail.com>
2019-02-26 20:26:56 -0600

    displace all products available

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,8 @@
 class Api::ProductsController < ApplicationController
+  def products_available
+    @dog = Product.all
+    @cat = Product.all
+    @parrot = Product.all
+    render "products_available.json.jbuilder"
+  end
 end
---
+++ app/views/api/products/products_available.json.jbuilder
@@ -0,0 +1,3 @@
+json.Product1 @dog.first
+json.Product2 @cat.second
+json.Product3 @parrot.third
